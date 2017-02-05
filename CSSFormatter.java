7797533800f40f70a0857498e573a583c3580290
Robert-Roy <robertproy@live.com>
2017-02-04 23:46:11 -0600

    initial commit

---
+++ CSSFormatter.java
@@ -0,0 +1,37 @@
+/*
+ * All rights reserved. Copyright Robert Roy 2016.
+ */
+package css.formatter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author Robert Roy <www.robertsworkspace.com>
+ */
+public class CSSFormatter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // adds linebreaks where appropriate in css files. after ;, {, and before and after }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String lineBreakCharacters[] = {";", "{", "}"};
+        String input = "";
+        try {
+            System.out.print("Give Data:");
+            input = br.readLine(); // text to search
+        } catch (IOException ex) {
+            System.out.println("This program has encountered an error.");
+            System.exit(0);
+        }
+        //System.out.println(prefix);
+        String output = input.replaceAll(";", ";\n");
+        output = output.replaceAll("\\{", "\\{\n");
+        output = output.replaceAll("\\}", "\n\\}\n");
+        System.out.println(output);
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,3 @@
+I got tired of looking at grotesque one line .css on websites so I made this.
+
+Adds linebreaks where appropriate in css files. after ";", "{", and before and after "}"
